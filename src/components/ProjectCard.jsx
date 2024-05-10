f7cfbac301e5d1d7d93c41b0bef359097669f506
Omar Corbic <omar_corbic@hotmail.com>
2024-05-10 21:11:24 +0200

    Moved project data to separate component

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Gallery from "./Gallery";
+
+const ProjectCard = ({ project = {} }) => {
+  return (
+    <article className=" flex flex-col items-center justify-center w-full lg:gap-14 gap-5  md:flex-row">
+      <div className=" text-center md:w-1/3 lg:items-start gap-3 md:text-left flex flex-col items-center justify-between">
+        <h4 className="text-[#00FF85] font-bold">{project.title}</h4>
+        <p>{project.description}</p>
+        <div className="flex justify-center md:justify-start text-xs w-full gap-2">
+          <a
+            target="_blank"
+            className="h-10 font-bold border-slate-900 dark:hover:bg-[#10f5a3] duration-150 w-full flex items-center justify-center border-2 px-4 rounded-xl"
+            href={project.sourceUrl}
+          >
+            Source code
+          </a>
+          <a
+            target="_blank"
+            className="h-10 font-bold border-slate-900 dark:hover:bg-[#10f5a3] duration-150 w-full flex items-center justify-center border-2 px-4 rounded-xl"
+            href={project.demoUrl}
+          >
+            View Live
+          </a>{" "}
+        </div>
+      </div>
+      <div className="rounded-xl overflow-hidden max-w-[700px]">
+        <Gallery photos={project.photos} />
+      </div>
+    </article>
+  );
+};
+
+export default ProjectCard;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,77 +1,75 @@
-import React, { useEffect, useState } from "react";
-import Button from "./Button";
+import React, { useEffect } from "react";
 import elecom1 from "../images/projects/elecommHomeDesktop.jpg";
 import elecom2 from "../images/projects/elecommLaptopsDesktop.jpg";
 import elecom3 from "../images/projects/elecommSingleDesktop.jpg";
 import ttt1 from "../images/projects/tttLoginDesktop.jpg";
 import ttt2 from "../images/projects/tttMenuDesktop.jpg";
 import ttt3 from "../images/projects/tttGameDesktop.jpg";
+import saltCity1 from "../images/projects/saltcity-1.png";
+import saltCity2 from "../images/projects/saltcity-2.png";
+import saltCity3 from "../images/projects/saltcity-3.png";
+import saltCity4 from "../images/projects/saltcity-4.png";
+import saltCity5 from "../images/projects/saltcity-5.png";
+import saltCity6 from "../images/projects/saltcity-6.png";
 import Gallery from "./Gallery";
 import SectionTitle from "./SectionTitle";
+import ProjectCard from "./ProjectCard";
 
-const ecommerce = [elecom1, elecom2, elecom3];
-const game = [ttt1, ttt2, ttt3];
+const elecomPhotos = [
+  { src: elecom1, alt: "Project photo" },
+  { src: elecom2, alt: "Project photo" },
+  { src: elecom3, alt: "Project photo" },
+];
+const superTTTphotos = [
+  { src: ttt1, alt: "Project photo" },
+  { src: ttt2, alt: "Project photo" },
+  { src: ttt3, alt: "Project photo" },
+];
+const saltCityPhotos = [
+  { src: saltCity1, alt: "Project photo" },
+  { src: saltCity2, alt: "Project photo" },
+  { src: saltCity3, alt: "Project photo" },
+  { src: saltCity4, alt: "Project photo" },
+  { src: saltCity5, alt: "Project photo" },
+  { src: saltCity6, alt: "Project photo" },
+];
 
 const Projects = () => {
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-  const isSmScreen = screenWidth >= 640;
-  const isLgScreen = screenWidth >= 1024;
-  const photoWidth = isLgScreen ? 600 : isSmScreen ? 500 : 300;
-
-  useEffect(() => {
-    const handleResize = () => {
-      setScreenWidth(window.innerWidth);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
+  const projects = [
+    {
+      id: 0,
+      title: "Elecom - e-commerce website",
+      description:
+        "An e-commerce website with login system, cart, wishlist, reviews and filters",
+      photos: elecomPhotos,
+      sourceUrl: "https://github.com/OmarCorbic/Electronics-E-commerce",
+      demoUrl: "#",
+    },
+    {
+      id: 1,
+      title: "Super TicTacToe",
+      description:
+        "An online browser game where you can play normal TicTacToe, or Super one that adds another layer",
+      photos: superTTTphotos,
+      sourceUrl: "https://github.com/OmarCorbic/Super-TicTacToe-Online-Game",
+      demoUrl: "#",
+    },
+    {
+      id: 2,
+      title: "Salt City Systems Website",
+      description: "A static website for web design & development agency",
+      photos: saltCityPhotos,
+      sourceUrl: "https://github.com/OmarCorbic/Salt-City-Systems-Website",
+      demoUrl: "#",
+    },
+  ];
   return (
     <section className="" id="projectsSection">
       <SectionTitle text="MY PROJECTS" />
-      <div className="flex flex-col gap-16 lg:px-10">
-        <article className=" flex flex-col items-center lg:justify-end lg:gap-14 gap-5  md:flex-row">
-          <div className=" text-center md:w-1/3 lg:items-start gap-3 md:text-left flex flex-col items-center justify-between">
-            <h4 className="text-[#00FF85] font-bold">Elecom</h4>
-            <p>
-              An e-commerce website with login system, cart, wishlist, reviews
-              and filters
-            </p>
-            <div className="flex justify-center text-xs w-full gap-2">
-              <a
-                target="_blank"
-                href="https://github.com/OmarCorbic/Electronics-E-commerce"
-              >
-                <Button className="lg:h-10 w-28" text="Source Code" />
-              </a>
-              <Button className="lg:h-10 w-28" text="View Demo" />
-            </div>
-          </div>
-          <Gallery photoWidth={photoWidth} photos={ecommerce} />
-        </article>
-        <article className=" flex flex-col items-center lg:justify-end lg:gap-14 gap-5  md:flex-row-reverse">
-          <div className="text-center md:w-1/3 lg:items-start gap-3 md:text-left flex flex-col items-center justify-between">
-            <h4 className="text-[#00FF85] font-bold">Super TicTacToe</h4>
-            <p>
-              An online browser game where you can play normal TicTacToe, or
-              Super one that adds another layer
-            </p>
-            <div className="flex justify-center text-xs w-full gap-2">
-              <a
-                target="_blank"
-                href="https://github.com/OmarCorbic/Super-TicTacToe-Online-Game"
-              >
-                <Button className=" lg:h-10 w-28" text="Source Code" />
-              </a>
-              <Button className=" lg:h-10 w-28" text="View Demo" />
-            </div>
-          </div>
-          <Gallery photoWidth={photoWidth} photos={game} />
-        </article>
+      <div className="flex flex-col gap-16 lg:px-10 items-center">
+        {projects.map((project) => {
+          return <ProjectCard key={project.id} project={project} />;
+        })}
       </div>
     </section>
   );
